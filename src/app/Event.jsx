5ffc024e6ca9aebc164b4a3c416b7e975b8b0e0a
nico_martin <mail@nicomartin.ch>
2020-10-09 19:24:32 +0200

    added button prop

--- src/app/Event.jsx
+++ src/app/Event.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { Button } from '../theme';
+
 const Event = ({ title, description, onConfirmEvent }) => {
   return (
     <div>
       <h2>{title}</h2>
       <p>{description}</p>
-      <button onClick={onConfirmEvent}>Weiter</button>
+      <Button timerKey={title} clickAfter={10} onClick={onConfirmEvent}>
+        Weiter
+      </Button>
     </div>
   );
 };
