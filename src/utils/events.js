6b24b617a278828276df61e48160a793b1746965
Raphi <raphael.antonietti@liip.ch>
2020-10-10 01:16:49 +0200

    typo and format fix

--- src/utils/events.js
+++ src/utils/events.js
@@ -2,7 +2,8 @@ import { ASSETS } from '@utils/constants';
 
 export const INIT_EVENT = {
   title: 'Super-Duper-Trader',
-  description: "Das ist das supercoole BEKB Spiel, bei dem deine Assets geschickt verwalten musst.\n\nDein Ziel ist es, bis zur Pensionierung stink reich zu sein. Passe dazu dein Portfolio geschickt auf reale Ereignisse an.\n\nSwipe jetzt auf den Icons um dein Startvermögen von 100k auf Immobilien, Aktien und Güter zu verteilen.",
+  description:
+    'Das ist das supercoole BEKB Spiel, bei dem deine Assets geschickt verwalten musst.\n\nDein Ziel ist es, bis zur Pensionierungstink reich zu sein. Passe dazu dein Portfolio geschickt auf reale Ereignisse an.\n\nSwipe jetzt auf den Icons um dein Startvermögen von 100k auf Immobilien, Aktien und Güter zu verteilen.',
   modifiers: {},
 };
 
