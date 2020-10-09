cd70b4595d20f2d13ad3e31b29a6083bd077c9ca
tim <tim@timschoch.com>
2020-10-10 00:57:23 +0200

    some styling

--- src/app/Event.jsx
+++ src/app/Event.jsx
@@ -22,8 +22,10 @@ const Event = ({ title, description, onConfirmEvent, isFirst }) => {
           )}
         {isFirst ? '' : <div className="event__newspaper__date">01. Januar 2020</div>}
       </div>
-      <h2>{title}</h2>
-      <p>{description}</p>
+      <div className="event__text">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
       <Button
         className="event__next"
         timerKey={title}
