07fc79b5a22232e5817c004f442bfe1255ddfb73
tim <tim@timschoch.com>
2020-10-09 22:36:21 +0200

    add empty icons

--- src/app/Asset.jsx
+++ src/app/Asset.jsx
@@ -5,10 +5,18 @@ import './Asset.css';
 const Asset = ({ assetKey, value, modifyAsset }) => {
   return (
     <p className="asset">
-      <img
-        className="asset__image--filled"
-        src={`/assets/static/${assetKey}.png`}
-      />
+      <div className="asset__icons">
+        <img
+          className="asset__image--filled"
+          src={`/assets/static/${assetKey}.png`}
+        />
+        <div className="asset__icon__empty">
+          <img
+            className="asset__image--empty"
+            src={`/assets/static/${assetKey}-empty.png`}
+          />
+        </div>
+      </div>
       <span className="asset__value">
         <ContUp preserveValue start={0} end={value} />
       </span>
