abbe0bd8461b9b571e3c42a1e7d11f4586fa54b3
tim <tim@timschoch.com>
2020-10-10 06:35:02 +0200

    styled konto

--- src/app/KontoH.jsx
+++ src/app/KontoH.jsx
@@ -3,17 +3,30 @@ import React from 'react';
 import './KontoH.css';
 
 const KontoH = ({ bank, assets }) => {
-  const bankWidth = React.useMemo(() => (100 / (bank + assets)) * bank, [
+  const max = 3000;
+  const totalWealth = Math.floor(bank + assets);
+  const bankWidth = React.useMemo(() => (100 / totalWealth) * bank, [
     bank,
     assets,
   ]);
+  const investRatio = Math.ceil(100 - bankWidth);
+  const barWidth = Math.min(totalWealth / max * 100, 100);
+  console.log('bank', bank);
+  console.log('assets', assets);
+  console.log('barWidth', barWidth);
 
   return (
     <div className="konto-h">
-      <div className="konto-h__bar">
-        <div className="konto-h__bank" style={{ width: `${bankWidth}%` }} />
+      <div className="konto-h__label">
+        <h3>Dein Vermögen {totalWealth}.-</h3>
+        <span>({investRatio}% investiert)</span>
       </div>
-    </div>
+      <div className="konto-h__bar">
+        <div className="konto-h__total" style={{ width: `${barWidth}%` }}>
+          <div className="konto-h__bank" style={{ width: `${bankWidth}%` }} />
+        </div>
+      </div>
+    </div >
   );
 };
 
