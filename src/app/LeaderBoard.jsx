9bfd5e071d2c4cd249271851d1262a6d02eddba9
Raphi <raphael.antonietti@liip.ch>
2020-10-10 03:25:41 +0200

    Leaderboard stuff

--- src/app/LeaderBoard.jsx
+++ src/app/LeaderBoard.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { getLeaderboard } from '../utils/api';
+import './LeaderBoard.css';
 
 const LeaderBoard = ({ value }) => {
-  return <p>You reached: {value}$</p>;
+  const [leaders, setLeaders] = React.useState([]);
+  React.useEffect(() => {
+    getLeaderboard.then(payload => {
+      setLeaders(payload.data);
+    });
+  }, []);
+  return (
+    <div>
+      <img className="logo" src={`/assets/static/logo.svg`} alt="bekb logo" />
+      <p>
+        {leaders
+          .sort(function(a, b) {
+            return a.score > b.score;
+          })
+          .map((leader, i) => {
+            return (
+              <p className="leaders">
+                <span className="leader-position">{i + 1}</span>{' '}
+                <span className="leader-name">{leader.name}</span>
+                <span className="leader-score">{leader.score}$</span>
+              </p>
+            );
+          })}
+      </p>
+    </div>
+  );
 };
 
 export default LeaderBoard;
