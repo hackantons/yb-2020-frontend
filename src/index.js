e8952fb7680ceea72191e751406e1508518e63a5
nico_martin <mail@nicomartin.ch>
2020-10-10 07:42:37 +0200

    vh fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './styles.css';
 import './App';
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.setTimeout(() => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}, 1000);
+
 window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
